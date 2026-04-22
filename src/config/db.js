ae9b8de8a93008b4b90239a04e1acbb0a9e26467
Ishaq Hussain (Isaac) <isaaclearns58@gmail.com>
2026-04-22 23:47:25 +0300

    chore: setting up db and migration

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+import pg from "pg";
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// function to test database connection
+async function getPgVersion() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query("SELECT version()");
+    console.log(result.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+
+getPgVersion(); // run to test connection
+
+export default pool;
